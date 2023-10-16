fdc455391797d8df267001358c1491a097c0bc33
Kostiantyn Talamaniuk <ktalamaniuk@playtika.com>
2023-10-16 20:00:15 +0300

    refactoring: chrome options provider

--- src/test/java/utils/Launcher.java
+++
@@ -1,8 +0,0 @@
-package utils;
-
-public class Launcher extends TestDataReader{
-    public static void main(String[] args) {
-        String expectedErrorMessage = TestDataReader.get("INVALID_PASSWORD_MESSAGE");
-        System.out.println(expectedErrorMessage);
-    }
-}
